219251a2195b70ad06b439892ebedb6d0cee7eb7
Alvise de Faveri <adefaveri@rev.ng>
2021-11-15 13:00:01 +0100

    Add decompilation test cases

---
+++ tests/analysis/Decompilation/dynamic_native/array_of_structs.c
@@ -0,0 +1,37 @@
+/*
+ * This file is distributed under the MIT License. See LICENSE.md for details.
+ */
+
+#include <memory.h>
+#include <stdint.h>
+
+typedef struct __attribute__((packed)) {
+  uint16_t first_uint16;
+  uint64_t second_uint64;
+} TestStruct;
+
+TestStruct global_struct[10];
+
+static TestStruct *getGlobalStructArray(void) {
+  return global_struct;
+}
+
+int main() {
+  TestStruct struct_array[10];
+  memcpy(struct_array, getGlobalStructArray(), sizeof(TestStruct) * 10);
+  uint16_t max_first = 0;
+  uint64_t max_second = 0;
+
+  for (int i = 0; i < 10; i++) {
+    uint16_t cur_first = struct_array[0].first_uint16;
+    uint64_t cur_second = struct_array[0].second_uint64;
+
+    if (cur_first > max_first)
+      max_first = cur_first;
+
+    if (cur_second > max_second)
+      max_second = cur_second;
+  }
+
+  return max_first + max_second;
+}
---
+++ tests/analysis/Decompilation/dynamic_native/linked_list.c
@@ -0,0 +1,55 @@
+/*
+ * This file is distributed under the MIT License. See LICENSE.md for details.
+ */
+
+#include <malloc.h>
+#include <stdint.h>
+
+struct __attribute__((packed)) Node {
+  uint32_t content_uint32;
+  struct Node *prev;
+  struct Node *next;
+};
+
+int getPrevious(struct Node *n) {
+  if (n->prev)
+    return n->prev->content_uint32;
+  return 0;
+}
+int getNext(struct Node *n) {
+  if (n->next)
+    return n->next->content_uint32;
+  return 0;
+}
+
+int main() {
+  struct Node *first = (struct Node *) malloc(sizeof(struct Node));
+  first->prev = NULL;
+
+  struct Node *cur = first;
+  for (int i = 0; i < 10; i++) {
+    struct Node *new_node = (struct Node *) malloc(sizeof(struct Node));
+    cur->next = new_node;
+    new_node->prev = cur;
+    new_node->next = NULL;
+    new_node->content_uint32 = i;
+    cur = cur->next;
+  }
+
+  int sum = 0;
+  cur = first;
+  while (cur) {
+    sum += getPrevious(cur);
+    sum += getNext(cur);
+    cur = cur->next;
+  }
+
+  cur = first;
+  while (cur) {
+    struct Node *next = cur->next;
+    free(cur);
+    cur = next;
+  }
+
+  return sum;
+}
