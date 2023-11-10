77ca1645149ce77392082ef0f5c4066aad22e865
Ivan Krysak <ivan@rev.ng>
2023-11-10 15:41:16 +0200

    Make aarch64 register output consistent with revng

--- share/revng/test/tests/abi/ucontext_wrapper.h
+++ share/revng/test/tests/abi/ucontext_wrapper.h
@@ -36,15 +36,17 @@ const char *register_names[] = { "gs",  "fs",  "es",     "ds",  "edi",
 #elif defined(TARGET_aarch64)
 #define ILLEGAL_INSTRUCTION_WORD 0xe7f0def0
 
-#define REGISTER_COUNT (NGREG - 3)
-#define REGISTER(context, i) ((ucontext_t *) (context))->uc_mcontext.regs[i]
+#define REGISTER_COUNT (NGREG - 2)
+#define REGISTER(context, i)                                     \
+  (i == NGREG - 3 ? ((ucontext_t *) (context))->uc_mcontext.sp : \
+                    ((ucontext_t *) (context))->uc_mcontext.regs[i])
 #define SP(context) ((ucontext_t *) (context))->uc_mcontext.sp
 #define PC(context) ((ucontext_t *) (context))->uc_mcontext.pc
 
 const char *register_names[] = {
   "x0",  "x1",  "x2",  "x3",  "x4",  "x5",  "x6",  "x7",  "x8",  "x9",  "x10",
   "x11", "x12", "x13", "x14", "x15", "x16", "x17", "x18", "x19", "x20", "x21",
-  "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x29", "x30", "x31"
+  "x22", "x23", "x24", "x25", "x26", "x27", "x28", "x29", "lr",  "sp"
 };
 
 #elif defined(TARGET_arm)
