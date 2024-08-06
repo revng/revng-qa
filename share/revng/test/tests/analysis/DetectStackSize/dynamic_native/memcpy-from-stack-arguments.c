120e3e3626fcd391d10aaf34f4656ae5ae7000c2
Alessandro Di Federico <ale@rev.ng>
2024-08-06 12:01:42 +0200

    for-detect-stack-size: tests for memcpy from stack

---
+++ share/revng/test/tests/analysis/DetectStackSize/dynamic_native/memcpy-from-stack-arguments.c
@@ -0,0 +1,27 @@
+#include <stdint.h>
+#include <stddef.h>
+
+typedef struct {
+  uint64_t data[512 / 8];
+} data_512_t;
+
+data_512_t global;
+
+void memcpy_from_stack_arguments(data_512_t argument) __attribute__((weak));
+void memcpy_from_stack_arguments(data_512_t argument) {
+  global = argument;
+}
+
+void backward_memcpy_from_stack_arguments(data_512_t argument) __attribute__((weak));
+void backward_memcpy_from_stack_arguments(data_512_t argument) {
+  for (size_t I = 0; I < 512 / 8; ++I) {
+    size_t inverse_index = 512 / 8 - I - 1;
+    global.data[inverse_index] = argument.data[inverse_index];
+  }
+}
+
+void main() {
+  data_512_t argument = {};
+  memcpy_from_stack_arguments(argument);
+  backward_memcpy_from_stack_arguments(argument);
+}
