795416b92289ab59e4196d092955d9f9020c4195
Alessandro Di Federico <ale@rev.ng>
2025-12-19 15:14:19 +0100

    for-collect-cfg: test Windows dynamic calls

---
+++ share/revng/test/tests/analysis/CollectCFG/windows/dynamic-call.c
@@ -0,0 +1,11 @@
+/*
+ * This file is distributed under the MIT License. See LICENSE.md for details.
+ */
+
+#include <stdlib.h>
+#include <Windows.h>
+
+int main() {
+  ExitProcess(1);
+  return 0;
+}
