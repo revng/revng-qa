2c0b646c007f363d6b35555c6861db978ca322e9
Alessandro Di Federico <ale@rev.ng>
2026-05-21 13:20:52 +0200

    segregate.c: test more things

--- share/revng/test/tests/analysis/SegregateStackAccesses/dynamic_native/segregate.c
+++ share/revng/test/tests/analysis/SegregateStackAccesses/dynamic_native/segregate.c
@@ -11,14 +11,32 @@ typedef struct {
   uint64_t B;
 } TwoIntegers;
 
+typedef struct {
+  uint64_t A;
+  uint64_t B;
+  uint64_t D;
+  uint64_t E;
+  uint64_t F;
+  uint64_t G;
+  uint64_t H;
+} ManyIntegers;
+
 // RawFunctionType with register arguments, all primitives
 uint64_t raw_primitives_on_registers(uint64_t A, uint64_t B) {
   return Unknown + A + B;
 }
 
+uint64_t call_raw_primitives_on_registers() {
+  return Unknown + raw_primitives_on_registers(1, 2);
+}
+
 // RawFunctionType with register arguments, with pointers
 uint64_t raw_pointers_on_registers(uint64_t *A, uint64_t *B) {
   return Unknown + *A + *B;
+}
+
+uint64_t call_raw_pointers_on_registers() {
+  return Unknown + raw_pointers_on_registers(&Unknown, &Unknown);
 }
 
 // RawFunctionType with stack arguments, all primitives
@@ -33,9 +51,17 @@ uint64_t raw_primitives_on_stack(uint64_t A,
   return Unknown + G + H;
 }
 
+uint64_t call_raw_primitives_on_stack() {
+  return Unknown + raw_primitives_on_stack(1, 2, 3, 4, 5, 6, 7, 8);
+}
+
 // CABIFunctionType with scalar arguments, only in registers
 uint64_t cabi_primitives_on_registers(uint64_t A, uint64_t B) {
   return Unknown + A + B;
+}
+
+uint64_t call_cabi_primitives_on_registers() {
+  return Unknown + cabi_primitives_on_registers(1, 2);
 }
 
 // CABIFunctionType with scalar arguments, both in registers and on the stack
@@ -50,9 +76,17 @@ uint64_t cabi_primitives_on_stack(uint64_t A,
   return Unknown + G + H;
 }
 
+uint64_t call_cabi_primitives_on_stack() {
+  return Unknown + cabi_primitives_on_stack(1, 2, 3, 4, 5, 6, 7, 8);
+}
+
 // CABIFunctionType with aggregate on registers
 uint64_t cabi_aggregate_on_registers(TwoIntegers Arg) {
   return Unknown + Arg.A + Arg.B;
+}
+
+uint64_t call_cabi_aggregate_on_registers() {
+  return Unknown + cabi_aggregate_on_registers((TwoIntegers){ 1, 2 });
 }
 
 // CABIFunctionType with aggregate on the stack
@@ -66,6 +100,10 @@ uint64_t cabi_aggregate_on_stack(uint64_t A,
   return Unknown + Arg.A + Arg.B;
 }
 
+uint64_t call_cabi_aggregate_on_stack() {
+  return Unknown + cabi_aggregate_on_stack(1, 2, 3, 4, 5, 6, (TwoIntegers){ 1, 2 });
+}
+
 // CABIFunctionType with aggregate partly in the registers and partly on the
 // stack
 uint64_t cabi_aggregate_on_stack_and_registers(uint64_t A,
@@ -77,15 +115,36 @@ uint64_t cabi_aggregate_on_stack_and_registers(uint64_t A,
   return Unknown + Arg.A + Arg.B;
 }
 
-typedef struct {
-  uint64_t A;
-  uint64_t B;
-  uint64_t D;
-  uint64_t E;
-  uint64_t F;
-  uint64_t G;
-  uint64_t H;
-} ManyIntegers;
+uint64_t call_cabi_aggregate_on_stack_and_registers() {
+  return Unknown + cabi_aggregate_on_stack_and_registers(1,
+                                                         2,
+                                                         3,
+                                                         4,
+                                                         5,
+                                                         (TwoIntegers){ 1, 2 });
+}
+
+TwoIntegers raw_return_small_aggregate(void) {
+  TwoIntegers result;
+  result.A = 124;
+  result.B = 123;
+  return result;
+}
+
+uint64_t call_raw_return_small_aggregate() {
+  return Unknown + raw_return_small_aggregate().B;
+}
+
+TwoIntegers cabi_return_small_aggregate(void) {
+  TwoIntegers result;
+  result.A = 124;
+  result.B = 123;
+  return result;
+}
+
+uint64_t call_cabi_return_small_aggregate() {
+  return Unknown + cabi_return_small_aggregate().B;
+}
 
 ManyIntegers cabi_return_big_aggregate(void) {
   ManyIntegers result;
@@ -93,25 +152,20 @@ ManyIntegers cabi_return_big_aggregate(void) {
   return result;
 }
 
-uint64_t caller() {
-  uint64_t Result = 0;
-  Result += raw_primitives_on_registers(1, 2);
-  Result += raw_pointers_on_registers(&Result, &Result);
-  Result += raw_primitives_on_stack(1, 2, 3, 4, 5, 6, 7, 8);
-  Result += cabi_primitives_on_registers(1, 2);
-  Result += cabi_primitives_on_stack(1, 2, 3, 4, 5, 6, 7, 8);
-  Result += cabi_aggregate_on_registers((TwoIntegers){ 1, 2 });
-  Result += cabi_aggregate_on_stack(1, 2, 3, 4, 5, 6, (TwoIntegers){ 1, 2 });
-  Result += cabi_aggregate_on_stack_and_registers(1,
-                                                  2,
-                                                  3,
-                                                  4,
-                                                  5,
-                                                  (TwoIntegers){ 1, 2 });
-  Result += cabi_return_big_aggregate().D;
-  return Unknown + Result;
+uint64_t call_cabi_return_big_aggregate() {
+  return Unknown + cabi_return_big_aggregate().D;
 }
 
 int main(int argc, char *argv[]) {
-  return caller();
+  return (call_raw_primitives_on_registers()
+          + call_raw_pointers_on_registers()
+          + call_raw_primitives_on_stack()
+          + call_cabi_primitives_on_registers()
+          + call_cabi_primitives_on_stack()
+          + call_cabi_aggregate_on_registers()
+          + call_cabi_aggregate_on_stack()
+          + call_cabi_aggregate_on_stack_and_registers()
+          + call_raw_return_small_aggregate()
+          + call_cabi_return_small_aggregate()
+          + call_cabi_return_big_aggregate());
 }
