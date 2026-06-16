ed255ce901ccde5a5948bc4c93cae031ed721c99
Alessandro Di Federico <ale@rev.ng>
2026-06-16 10:20:20 +0200

    Revert "s/clang --target/${TRIPLE}-clang/g"
    This reverts commit 032ab091a784ed363082369010ca13a38387536a.

--- share/revng/test/tests/abi/prepare-foreign-executable.sh
+++ share/revng/test/tests/abi/prepare-foreign-executable.sh
@@ -64,14 +64,18 @@ function prepare-apple-foreign-executable() {
   INPUT_DIRECTORY="$5"
   OUTPUT_DIRECTORY="$6"
 
-  ${TRIPLE}-clang -c \
+  # TODO: add a symlink from `${TRIPLE}clang` to `clang --target=${TRIPLE}`
+  #       and use that instead
+  clang -c \
+    --target=${TRIPLE} \
     ${CFLAGS} -O3 \
     -fno-zero-initialized-in-bss -ffreestanding \
     -fno-optimize-sibling-calls -fno-pic \
     -Wno-unused-command-line-argument \
     "${INPUT_DIRECTORY}/setup.c" \
     -o "${OUTPUT_DIRECTORY}/foreign-executable.o"
-  ${TRIPLE}-clang -c \
+  clang -c \
+    --target=${TRIPLE} \
     ${CFLAGS} -O3 \
     -fno-zero-initialized-in-bss -ffreestanding \
     -nostdlib -nodefaultlibs -fno-pic \
